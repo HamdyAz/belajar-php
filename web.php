8a558168c1071f2666e54fec48fc0429a16f3cdd
HamdyAz <70446830+HamdyAz@users.noreply.github.com>
2020-11-19 10:55:16 +0700

    Laravel Template

---
+++ web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', 'HomeController@index');
+
+Route::post('/login', 'HomeController@login');
+
+Route::get('/master', function() {
+    return view('adminlte.master');
+});
+
+Route::get('/items/index', function() {
+    return view('items.index');
+});
+
+Route::get('/items/create', function() {
+    return view('items.create');
+});
+
+
